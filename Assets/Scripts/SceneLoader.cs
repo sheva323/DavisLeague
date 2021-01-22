13f5b18605cfae05fb3853a3b9e8b893e30c7461
sheva323 <sheva3232@gmail.com>
2021-01-21 19:29:06 -0500

    update 21.01.2021

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadProfileScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void LoadA0Scene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadA1Scene()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void LoadA2Scene()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
